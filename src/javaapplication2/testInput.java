19cc3be824bc42f7c726074dac0ab9519799c6b5
Fajar Satria <ryansat46@gmail.com>
2018-10-02 16:42:36 +0700

    joptionpane

---
+++ src/javaapplication2/testInput.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class testInput {
+    public static void main(String[] test12){
+        int a;
+        a = Integer.parseInt(JOptionPane.showInputDialog("Masuuka Nilai"));
+        System.out.println(a);
+    }
+}
